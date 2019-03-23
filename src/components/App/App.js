92677d4d2f915df35b99b7b4f6f329e0608dc80c
Minh Pham <minhpham@Minhs-MacBook-Pro.local>
2019-03-23 20:49:13 +0700

    Add remove button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import DATA from "./data";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
@@ -15,15 +16,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: DATA.data
     };
+    const that = this;
     this.columnDefs = [
       {
         headerName: "Truck plate",
         field: "truck_plate",
         width: 120,
         cellRenderer: function(params) {
-          return params.data.truck_plate || "";
+          const div = document.createElement("div");
+          div.innerHTML = params.data.truck_plate || "";
+          const img = document.createElement("span");
+          img.innerHTML = "remove";
+          div.appendChild(img);
+          img.onclick = function() {
+            that.state.data.map((item, index) => {
+              if (item.truck_plate === params.data.truck_plate) {
+                that.state.data.splice(index, 1);
+                return;
+              }
+            });
+            const newData = that.state.data.slice(0, NUMBERS_OF_ROW_DATA);
+            that.gridApi.setRowData(newData);
+            that.setState({
+              data: that.state.data
+            });
+          };
+
+          return div;
         }
       },
       {
@@ -102,6 +123,8 @@ class App extends React.Component {
     this.defaultColDef = {};
   }
 
+  handleRemove(params) {}
+
   onGridReady(params) {
     this.gridApi = params.api;
   }
@@ -116,13 +139,12 @@ class App extends React.Component {
   }
 
   handleCallBackSubmit(addedData) {
-    console.log(addedData);
     const data = DATA.data;
     data.unshift(addedData);
     const newData = data.slice(0, NUMBERS_OF_ROW_DATA);
     this.gridApi.setRowData(newData);
     this.setState({
-      data: data
+      data
     });
   }
 
